3e373d60682b34f1c1a26d354ef4bf4645cc70e8
Jala Unterreiner <jala.binbek@outlook.com>
2022-05-31 15:33:35 -0400

    cleaned up js, added comments for clarity

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-var currentTime = moment().format("MMMM Do YYYY")
-var containerEl = $(".container")
-console.log("hello!");
-// WHEN I scroll down
-// THEN I am presented with time blocks for standard business hours
-// WHEN I view the time blocks for that day
-// THEN each time block is color-coded to indicate whether it is in the past, present, or future
+//Function to display colors based on the hour of the day, tracked through Moment.
 function showColors() {
+    //Moment pulls the users current hour.
     var hour = moment().hours();
     $(".time-block").each(function () {
+        //Color changes based on the parameters set in the if/else statements.
         var rowTime = parseInt($(this).attr("id"));
         if (rowTime < hour) {
             $(this).addClass("past");
+            //Based on set parameters, colors (set through css) will change.
         } else if (rowTime === hour) {
             $(this).removeClass("past");
             $(this).addClass("present");
@@ -25,18 +19,24 @@ function showColors() {
         }
     })
 };
+//Calls the function.
 showColors();
 
+//Save button click event 
 $(".saveBtn").on("click", function () {
-    console.log(this)
+    //Variables to set the hour and user input in local storage
     var input = $(this).siblings(".event").val();
     var time = $(this).parent().attr("id");
     localStorage.setItem(time, input);
 }) 
+//Stores text and time in local storage. Pulls from event class inside the textarea html
 $("#9 .event").val(localStorage.getItem("9"));
-// WHEN I click into a time block
-// THEN I can enter an event
-// WHEN I click the save button for that time block
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
+$("#10 .event").val(localStorage.getItem("10"));
+$("#11.event").val(localStorage.getItem("11"));
+$("#12 .event").val(localStorage.getItem("12"));
+$("#13 .event").val(localStorage.getItem("13"));
+$("#14 .event").val(localStorage.getItem("14"));
+$("#15 .event").val(localStorage.getItem("15"));
+$("#16 .event").val(localStorage.getItem("16"));
+$("#17 .event").val(localStorage.getItem("17"));
+$("#18 .event").val(localStorage.getItem("18"));
